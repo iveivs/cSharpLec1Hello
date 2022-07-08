f5cb72807be3b3932f24667a350bd2f436e354e6
veiv <veiv@mail.ru>
2022-07-09 02:12:28 +0300

    add Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.To.Lower() == "Светослав")
+{
+    Console.WriteLine("Ура, это же СВЕТОСЛАВ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
